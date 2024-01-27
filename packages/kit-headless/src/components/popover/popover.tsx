cd1ddd10e2bbcf818a8623e235e30c3a355258af
web2integrators <jay@officeintegrator.com>
2024-01-26 22:59:45 -0500

    added placement prop for tooltip

--- packages/kit-headless/src/components/popover/popover.tsx
+++ packages/kit-headless/src/components/popover/popover.tsx
@@ -5,7 +5,7 @@ import { PopoverImplProps } from './popover-impl';
 import { FloatingProps } from './floating';
 
 // TODO: improve the type so that it only includes FloatingProps when floating is true.
-type PopoverProps = PopoverImplProps & { floating?: boolean } & FloatingProps;
+export type PopoverProps = PopoverImplProps & { floating?: boolean } & FloatingProps;
 
 /* This component determines whether the popover needs floating behavior, a common example where it doesn't, would be a toast. */
 export const Popover = component$<PopoverProps>((props) => {
--- packages/kit-headless/src/components/tooltip/tooltip-popover.tsx
+++ packages/kit-headless/src/components/tooltip/tooltip-popover.tsx
@@ -1,24 +1,26 @@
 import { component$, PropsOf, Slot, useContext } from '@builder.io/qwik';
-import { Popover } from '@qwik-ui/headless';
+import { Popover, PopoverProps } from '@qwik-ui/headless';
 import { tooltipContextId } from './tooltip-context';
 
-export type TooltipPopoverProps = PropsOf<'div'>;
+export type TooltipPopoverProps = PropsOf<'div'> & PopoverProps;
 
-export const TooltipPopover = component$(({ ...props }: TooltipPopoverProps) => {
-  const context = useContext(tooltipContextId);
-  const popoverId = `${context.localId}-popover`;
+export const TooltipPopover = component$(
+  ({ placement = 'top', ...props }: TooltipPopoverProps) => {
+    const context = useContext(tooltipContextId);
+    const popoverId = `${context.localId}-popover`;
 
-  return (
-    <Popover
-      {...props}
-      ref={context.popoverRef}
-      anchorRef={context.triggerRef}
-      floating={true}
-      id={popoverId}
-      placement="top"
-      manual
-    >
-      <Slot />
-    </Popover>
-  );
-});
+    return (
+      <Popover
+        {...props}
+        ref={context.popoverRef}
+        anchorRef={context.triggerRef}
+        floating={true}
+        id={popoverId}
+        placement={placement}
+        manual
+      >
+        <Slot />
+      </Popover>
+    );
+  },
+);
