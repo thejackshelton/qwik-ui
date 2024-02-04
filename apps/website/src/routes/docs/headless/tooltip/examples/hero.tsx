21a33c410540ada6534a1580d257adc216f82a3f
Diedo Díaz <diegoddiaz05@gmail.com>
2024-02-03 20:33:51 -0500

    docs(tooltip): update tooltip docs

--- apps/website/src/routes/docs/headless/tooltip/examples/hero.tsx
+++ apps/website/src/routes/docs/headless/tooltip/examples/hero.tsx
@@ -4,9 +4,11 @@ import { Tooltip, TooltipPopover, TooltipTrigger } from '@qwik-ui/headless';
 export default component$(() => {
   return (
     <Tooltip>
-      <TooltipTrigger popovertarget="hero-tooltip">Trigger!</TooltipTrigger>
-      <TooltipPopover class="m-0 bg-blue-600" id="hero-tooltip">
-        Popover!
+      <TooltipTrigger class="hover:text-accent-foreground px-2 py-1 underline underline-offset-2 hover:font-medium">
+        Hover me
+      </TooltipTrigger>
+      <TooltipPopover class="bg-muted text-muted-foreground textt-sm m-0 mb-2 rounded-lg px-3 py-2">
+        Hi! This is the tooltip content. 😀
       </TooltipPopover>
     </Tooltip>
   );
--- apps/website/src/routes/docs/headless/tooltip/snippets/building-blocks.tsx
+++ apps/website/src/routes/docs/headless/tooltip/snippets/building-blocks.tsx
@@ -1,6 +1,13 @@
 import { component$ } from '@builder.io/qwik';
-import { Tooltip } from '@qwik-ui/headless';
+import { Tooltip, TooltipPopover, TooltipTrigger } from '@qwik-ui/headless';
 
 export default component$(() => (
-  <Tooltip content="Tooltip content">Trigger content</Tooltip>
+  <Tooltip>
+    <TooltipTrigger class="hover:text-accent-foreground px-2 py-1 underline underline-offset-2 hover:font-medium">
+      Hover me
+    </TooltipTrigger>
+    <TooltipPopover class="bg-muted text-muted-foreground textt-sm m-0 mb-2 rounded-lg px-3 py-2">
+      Hi! This is the tooltip content. 😀
+    </TooltipPopover>
+  </Tooltip>
 ));
