aa3f2a948a37f7c1ce71c97a8526ecf257859b76
jack shelton <me@jackshelton.com>
2024-07-12 11:13:42 -0500

    tabs tests

---
+++ apps/website/src/routes/docs/headless/tabs/examples/programmatic.tsx
@@ -0,0 +1,35 @@
+import { component$, useSignal, useStyles$ } from '@builder.io/qwik';
+import { Tabs } from '@qwik-ui/headless';
+import styles from '.././index.css?inline';
+
+export default component$(() => {
+  useStyles$(styles);
+
+  const selectedIndex = useSignal(0);
+
+  return (
+    <>
+      <div class="tabs-example">
+        <h3>Danish Composers</h3>
+        <Tabs.Root bind:selectedIndex={selectedIndex} behavior="automatic">
+          <Tabs.List>
+            <Tabs.Tab>Maria</Tabs.Tab>
+            <Tabs.Tab>Carl</Tabs.Tab>
+            <Tabs.Tab>Ida</Tabs.Tab>
+          </Tabs.List>
+          <Tabs.Panel>
+            <p>Maria Theresia Ahlefeldt (16 January 1755 - 20 December 1810) ...</p>
+          </Tabs.Panel>
+          <Tabs.Panel>
+            <p>Carl Joachim Andersen (29 April 1847 - 7 May 1909) ...</p>
+          </Tabs.Panel>
+          <Tabs.Panel>
+            <p>Ida Henriette da Fonseca (July 27, 1802 - July 6, 1858) ...</p>
+          </Tabs.Panel>
+        </Tabs.Root>
+      </div>
+
+      <button onClick$={() => (selectedIndex.value = 1)}>change to 2nd tab</button>
+    </>
+  );
+});
--- packages/kit-headless/src/components/tabs/tabs.test.ts
+++ packages/kit-headless/src/components/tabs/tabs.test.ts
@@ -16,11 +16,13 @@ test.describe('A11y', () => {
   test('Axe Validation Test', async ({ page }) => {
     await setup(page, 'hero');
 
-    const initialResults = await new AxeBuilder({ page })
-      .include('[role="combobox"]')
+    const a11yScanResults = await new AxeBuilder({ page })
+      .include('[role="tablist"]')
+      .include('[role="tab"]')
+      .include('[role="tabpanel"]')
       .analyze();
 
-    expect(initialResults.violations).toEqual([]);
+    expect(a11yScanResults.violations).toEqual([]);
   });
 });
 
@@ -33,5 +35,27 @@ test.describe('Critical functionality', () => {
     await d.getTabAt(1).click();
 
     await expect(d.getVisiblePanel()).toContainText('Carl');
+  });
+
+  test(`GIVEN 3 tabs
+        WHEN changing the selected index programmatically to the middle
+        THEN render the middle panel`, async ({ page }) => {
+    const { driver: d } = await setup(page, 'programmatic');
+
+    await expect(d.getVisiblePanel()).toContainText('Maria');
+
+    await page.getByRole('button', { name: 'change to 2nd tab' }).click();
+
+    await expect(d.getVisiblePanel()).toContainText('Carl');
+  });
+
+  test(`GIVEN 3 tabs
+        WHEN clicking the middle one
+        THEN onSelectedIndexChange should be called`, async ({ page }) => {
+    const { driver: d } = await setup(page, 'on-selected-index-change');
+
+    await d.getTabAt(1).click();
+
+    await expect(page.getByRole('paragraph').last()).toHaveText('Selected Index: 1');
   });
 });
